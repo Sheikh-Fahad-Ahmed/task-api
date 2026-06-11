eee77ad41b6297f3bdfb158dad0e98f80b00e49d
Fahad Ahmed <fahad838@gmail.com>
2026-06-11 16:28:31 +0530

    Feat: implement Update() store logic

--- store/store.go
+++ store/store.go
@@ -51,3 +51,37 @@ func GetByID(idString string) (models.Task, error) {
 	}
 	return models.Task{}, errors.New("Task does not exists")
 }
+
+func Update(idString string, taskInput models.TaskInput) (models.Task, error) {
+	id, err := strconv.Atoi(idString)
+	if err != nil {
+		return models.Task{}, errors.New("unable to convert id from string -> int")
+	}
+
+	var title, description, status string
+
+	for i, task := range tasks {
+		if task.ID == id {
+			if taskInput.Title != "" {
+				title = taskInput.Title
+			} else {
+				title = task.Title
+			}
+			if taskInput.Description != "" {
+				description = taskInput.Description
+			} else {
+				description = task.Description
+			}
+			if taskInput.Status != "" {
+				status = taskInput.Status
+			} else {
+				status = task.Status
+			}
+			tasks[i] = *models.New(id, title, description, status)
+			tasks[i].CreatedAt = time.Now()
+			return tasks[i], nil
+		}
+	}
+
+	return models.Task{}, errors.New("Task not Found..")
+}
