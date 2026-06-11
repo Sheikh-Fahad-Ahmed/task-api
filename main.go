73565528bacf9d6d194b2b238f02e349610f4f9e
Fahad Ahmed <fahad838@gmail.com>
2026-06-11 08:47:04 +0530

    Feat: define GET, POST, PUT and DELETE endpoints

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/Sheikh-Fahad-Ahmed/task-api/handlers"
+	"github.com/gorilla/mux"
+)
+
 func main(){
-	
+	r := mux.NewRouter()
+
+	r.HandleFunc("/tasks", handlers.GetTasks).Methods("GET")
+	r.HandleFunc("/tasks/{id}", handlers.GetTasksByID).Methods("GET")
+
+	r.HandleFunc("/tasks", handlers.CreateTask).Methods("POST")
+
+	r.HandleFunc("/tasks/{id}", handlers.UpdateTask).Methods("PUT")
+
+	r.HandleFunc("/tasks/{id}", handlers.DeleteTask).Methods(("DELETE"))
+
+	http.ListenAndServe(":8080", r)
+
 }
