5b0cf46c8d9d2dcd720f4c4b1ce9d11ed5c66d1a
Fahad Ahmed <fahad838@gmail.com>
2026-06-11 08:58:00 +0530

    Feat: implement GetTasks() logic to return all tasks

--- handlers/task_handlers.go
+++ handlers/task_handlers.go
@@ -1,2 +1,17 @@
 package handlers
 
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Sheikh-Fahad-Ahmed/task-api/store"
+)
+
+func GetTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	tasks := store.GetAll() 
+
+	json.NewEncoder(w).Encode(tasks)
+}
+
