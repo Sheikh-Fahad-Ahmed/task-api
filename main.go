3852bac460210f8059e8c32dbf34dedd163ebf6c
Fahad Ahmed <fahad838@gmail.com>
2026-06-11 17:00:28 +0530

    ReFactor: Rename handler functions
    DeleteTasks() -> DeleteTask()
    GetTasksById() -> GetTaskByID()

--- main.go
+++ main.go
@@ -11,13 +11,13 @@ func main(){
 	r := mux.NewRouter()
 
 	r.HandleFunc("/tasks", handlers.GetTasks).Methods("GET")
-	r.HandleFunc("/tasks/{id}", handlers.GetTasksByID).Methods("GET")
+	r.HandleFunc("/tasks/{id}", handlers.GetTaskByID).Methods("GET")
 
 	r.HandleFunc("/tasks", handlers.CreateTask).Methods("POST")
 
 	r.HandleFunc("/tasks/{id}", handlers.UpdateTask).Methods("PUT")
 
-	r.HandleFunc("/tasks/{id}", handlers.DeleteTask).Methods(("DELETE"))
+	r.HandleFunc("/tasks/{id}", handlers.DeleteTask).Methods("DELETE")
 
 	http.ListenAndServe(":8080", r)
 
