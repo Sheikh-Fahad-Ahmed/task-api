443ea0b5351229ec54f9e2fb099add94859baf62
Fahad Ahmed <fahad838@gmail.com>
2026-06-11 08:26:28 +0530

    Implement: New() to create a new task

---
+++ models/task.go
@@ -0,0 +1,21 @@
+package models
+
+import "time"
+
+type Task struct {
+	ID          int       `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+}
+
+func New(id int, title string, description string, status string) *Task {
+	return &Task{
+		ID:          id,
+		Title:       title,
+		Description: description,
+		Status:      status,
+		CreatedAt:   time.Now(),
+	}
+}
