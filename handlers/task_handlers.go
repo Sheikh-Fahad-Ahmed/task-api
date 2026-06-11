1fd94e3e55c790940ee4d09c5d631ba8116339bd
Fahad Ahmed <fahad838@gmail.com>
2026-06-11 16:27:31 +0530

    Feat: implement UpdateTask() handler

--- handlers/task_handlers.go
+++ handlers/task_handlers.go
@@ -59,3 +59,27 @@ func GetTaskByID(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(task)
 }
+
+func UpdateTask(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	idString := vars["id"]
+	w.Header().Set("Content-Type", "application/json")
+
+	var TaskInput models.TaskInput
+	err := json.NewDecoder(r.Body).Decode(&TaskInput)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"err": "unable to decode request body"})
+		return
+	}
+
+	task, err := store.Update(idString, TaskInput)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(task)
+
+}
