5d15ed70be9993fed3eb2b9f8af761083436d244
Fahad Ahmed <fahad838@gmail.com>
2026-06-17 21:12:13 +0530

    Refactor: Refactor GetTaskByID to use Gin

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ func main() {
 
 	r.GET("/tasks", handlers.GetTasks)
 	// r.HandleFunc("/tasks/{id}", handlers.GetTaskByID).Methods("GET")
+	r.GET("tasks/:id", handlers.GetTaskByID)
 
 	r.POST("/tasks", handlers.CreateTask)
 
