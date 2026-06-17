d24dace46e49a21a70dc5b89525316fb053a77d2
Fahad Ahmed <fahad838@gmail.com>
2026-06-17 08:56:06 +0530

    Feat: implement logger logic

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/Sheikh-Fahad-Ahmed/task-api/handlers"
+	"github.com/Sheikh-Fahad-Ahmed/task-api/logger"
 	"github.com/gorilla/mux"
 )
 
-func main(){
+func main() {
+	log.SetFlags(log.Ldate | log.Ltime )
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/tasks", handlers.GetTasks).Methods("GET")
@@ -19,6 +23,15 @@ func main(){
 
 	r.HandleFunc("/tasks/{id}", handlers.DeleteTask).Methods("DELETE")
 
-	http.ListenAndServe(":8080", r)
+	loggedRoute := logger.Logging(r.ServeHTTP)
+
+	http.ListenAndServe(":8080", loggedRoute)
 
 }
+
+/*
+Middleware: add a custom logging middleware (wrap http.Handler to log method, path, and duration) and explain in your README why logging matters
+Input sanitization: trim whitespace from title before storing it
+Query filter: GET /tasks?status=done filters by status on the list endpoint
+
+*/
