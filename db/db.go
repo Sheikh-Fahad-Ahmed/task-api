3480e7789ad52e861069289f841ea73b2fee8ea0
Fahad Ahmed <fahad838@gmail.com>
2026-06-18 11:16:05 +0530

    Fix: fix sql.Open to use path dynamically

--- db/db.go
+++ db/db.go
@@ -7,7 +7,7 @@ import (
 )
 
 func InitDB(path string) (*sql.DB, error){
-	db ,err := sql.Open("sqlite3", "./tasks.db")
+	db ,err := sql.Open("sqlite3", path)
 	if err != nil {
 		return nil, err
 	}
