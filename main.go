e834b9b2f05a4ea16f1a4d4fa08a978128ada8ee
Fahad Ahmed <fahad838@gmail.com>
2026-06-21 21:09:57 +0530

    Feat: Add logic to create logging -> log file

--- main.go
+++ main.go
@@ -1,40 +1,46 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
+	"os"
 
 	"github.com/Sheikh-Fahad-Ahmed/task-api/db"
 	"github.com/Sheikh-Fahad-Ahmed/task-api/handlers"
+	"github.com/Sheikh-Fahad-Ahmed/task-api/logger"
 	"github.com/Sheikh-Fahad-Ahmed/task-api/store"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	log.SetFlags(log.Ldate | log.Ltime)
+	logFile := "gin.log"
+
 	database, err := db.InitDB("./tasks.db")
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	taskStore := store.New(database)
-
 	taskHandler := handlers.New(taskStore)
 
-	r := gin.Default()
+	gin.DisableConsoleColor()
+	f, _ := os.Create(logFile)
+	gin.DefaultWriter = io.MultiWriter(f)
+	log.SetOutput(gin.DefaultWriter)
+
+	r := gin.New()
+	r.Use(logger.CustomLogger())
+	r.Use(gin.Recovery())
 
 	r.GET("/tasks", taskHandler.GetTasks)
-
-	r.GET("tasks/:id", taskHandler.GetTaskByID)
-
+	r.GET("/tasks/:id", taskHandler.GetTaskByID)
 	r.POST("/tasks", taskHandler.CreateTask)
-
 	r.PUT("/tasks/:id", taskHandler.UpdateTask)
+	r.DELETE("/tasks/:id", taskHandler.DeleteTask)
 
-	r.DELETE("tasks/:id", taskHandler.DeleteTask)
+	fmt.Println("server starting on http://localhost:8080")
+	fmt.Printf("\nAll logs are routed to: %s", logFile)
 
-	// loggedRoute := logger.Logging(r.ServeHTTP)
-
-	log.Printf("Server is running on http://localhost:8080")
-	r.Run()
+	r.Run(":8080")
 
 }
